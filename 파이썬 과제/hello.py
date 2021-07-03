036e7995c95da96550acd6b76fb1b0c306dfea25
Jimin Kim <0305jimin@gmail.com>
2021-07-04 01:50:57 +0900

    Unit 13

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,7 +1,4 @@
-x = int(input())         
+x = 5
  
-if x == 10:             
-    print('10입니다.')   
- 
-if x == 20:             
-    print('20입니다.')   
+if x!=10:                        
+    print('ok')
