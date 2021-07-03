60f1033adbd00a8c78b448d2a37261d9ac2ae8fd
Jimin Kim <0305jimin@gmail.com>
2021-07-03 17:48:20 +0900

    7.3, 7.3 오답 정리

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,3 +1,30 @@
-print(1, end='')
-print(2,end='')
-print(3)
+# 1. 다음 중 3.1 Python 100을 한 줄에 출력하는 방법으로 올바른 것을 고르세요.
+
+# print(3.1 Python 100)->Invalid syntax 문자열 출력은 ''안에 있어야 한다.
+
+# print(3.1)
+# print('Python')
+# print(100)-> 출력은 되지만 세 줄로 출력된다.
+
+# print(3.1, Python, 100)-> 문자열은 ''안에 있어야 한다.
+
+# print(3.1, 'Python', 100)->정답
+
+# print(3.1; 'Python'; 100)-> 여러 개 출력 때 ;말고 ,
+
+
+
+
+# 3. 다음 중 'Hello'와 'Python'을 두 줄로 출력하는 방법으로 올바른 것을 모두 고르세요.
+
+# Hello
+# Python
+
+
+# print('Hello', 'Python')-> 한 줄
+# print('Hello\nPython')->정답
+# print('Hello', 'Python', end='\n')->한 줄
+# print('Hello', 'Python', sep='\n')->정답
+# print('Hello', '\n', 'Python', sep='')->정답 이스케이프 문자도 문자라서 ''안에.
+
+
