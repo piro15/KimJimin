095fe4b9ab682ed4bd9b73691e45c2d6f9d23b2a
Jimin Kim <0305jimin@gmail.com>
2021-07-04 01:22:46 +0900

    Unit 12

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,3 +1,15 @@
-n = -32, 75, 97, -10, 9, 32, 4, -15, 0, 76, 14, 2
+camille = {
+    'health': 575.6,
+    'health_regen': 1.7,
+    'mana': 338.8,
+    'mana_regen': 1.63,
+    'melee': 125,
+    'attack_damage': 60,
+    'attack_speed': 0.625,
+    'armor': 26,
+    'magic_resistance': 32.1,
+    'movement_speed': 340
+}
 
-print(n[1::2])
+print(camille['health'])
+print(camille['movement_speed'])
