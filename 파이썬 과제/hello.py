243b802b788ca3037b26197ca401a3499fa55cd6
Jimin Kim <0305jimin@gmail.com>
2021-07-03 11:59:37 +0900

    4.2

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,3 +1,11 @@
+#Hello, world! 출력
 print('Hello world!')
-print('Python Programming')
-print('Hello'); print('1234')
+
+#print('Hello, world!')
+
+a=1+2 #더하기
+print('Hello, world!') #printf('1234567890')
+
+# 더하기
+# a=1+2
+# print('Hello, world!')
