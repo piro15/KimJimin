28a51c70d8c21524cbf318f468fbfc78c04cd76f
Jimin Kim <0305jimin@gmail.com>
2021-07-04 01:47:41 +0900

    13.4

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,10 +1,7 @@
-x=15
-
-if x >= 10:
-    print('10 이상입니다.')
+x = int(input())         
  
-    if x == 15:
-        print('15입니다.') 
+if x == 10:             
+    print('10입니다.')   
  
-    if x == 20:
-        print('20입니다.') 
+if x == 20:             
+    print('20입니다.')   
