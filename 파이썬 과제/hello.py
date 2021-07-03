7813a6dc0a5d7e3051d78b23240b046b33800b96
Jimin Kim <0305jimin@gmail.com>
2021-07-04 02:08:07 +0900

    14.3

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,7 +1,40 @@
-x = 5
- 
-if x == 10:
-    print('10입니다.') 
+if True:
+    print('참')   
 else:
-    print('x에 들어있는 숫자는')    
-    print('10이 아닙니다.')
+    print('거짓')
+ 
+if False:
+    print('참')
+else:
+    print('거짓')    
+ 
+if None:
+    print('참')
+else:
+    print('거짓')
+
+
+if 0:
+    print('참')
+else:
+    print('거짓')    # 0은 거짓
+ 
+if 1:
+    print('참')    # 1은 참
+else:
+    print('거짓')
+ 
+if 0x1F:    # 16진수
+    print('참')    # 0x1F는 참
+else:
+    print('거짓')
+ 
+if 0b1000:    # 2진수
+    print('참')    # 0b1000은 참
+else:
+    print('거짓')
+ 
+if 13.5:    # 실수
+    print('참')    # 13.5는 참
+else:
+    print('거짓')
