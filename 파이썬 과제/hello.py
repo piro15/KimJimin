cbb561a8791f3d4b4dfadb5c37de860b85c7d768
Jimin Kim <0305jimin@gmail.com>
2021-07-04 02:17:06 +0900

    Unit 14

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,40 +1,7 @@
-if True:
-    print('참')   
-else:
-    print('거짓')
+written_test = 75
+coding_test = True
  
-if False:
-    print('참')
+if written_test >= 80 and coding_test == True:
+    print('합격')
 else:
-    print('거짓')    
- 
-if None:
-    print('참')
-else:
-    print('거짓')
-
-
-if 0:
-    print('참')
-else:
-    print('거짓')    # 0은 거짓
- 
-if 1:
-    print('참')    # 1은 참
-else:
-    print('거짓')
- 
-if 0x1F:    # 16진수
-    print('참')    # 0x1F는 참
-else:
-    print('거짓')
- 
-if 0b1000:    # 2진수
-    print('참')    # 0b1000은 참
-else:
-    print('거짓')
- 
-if 13.5:    # 실수
-    print('참')    # 13.5는 참
-else:
-    print('거짓')
+    print('불합격')
