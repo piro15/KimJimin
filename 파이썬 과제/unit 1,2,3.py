be21054d36eb718bd386ea1032dcdea234e29179
Jimin Kim <0305jimin@gmail.com>
2021-07-03 11:16:40 +0900

    Unit1, Unit2,Unit 3-1, 3-2

---
+++ 파이썬 과제/unit 1,2,3.py
@@ -0,0 +1,13 @@
+Python 3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:49:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello world!')
+Hello world!
+>>> print(Hello,world)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(Hello,world)
+NameError: name 'Hello' is not defined
+>>> print('Hello,world')
+Hello,world
+>>> 
