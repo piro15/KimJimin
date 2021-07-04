badd23dfd478966f12a357d54c117f849b605b47
Jimin Kim <0305jimin@gmail.com>
2021-07-04 16:05:06 +0900

    Unit 19

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,7 +1,10 @@
-for i in range(5):
-    for j in range(5):
-        if j<i:
+h=int(input())
+for i in range(h):
+    for j in reversed(range(h)):
+        if j>i:
             print(' ',end='')
-        else:
+        elif j==i:
+            print('*',end='')
+        elif j<i:
             print('*',end='')
     print()
