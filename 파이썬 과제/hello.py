9b2d97ad249542b4311672a58b755f8551737c56
Jimin Kim <0305jimin@gmail.com>
2021-07-04 16:16:54 +0900

    20.5

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,10 +1,24 @@
-h=int(input())
-for i in range(h):
-    for j in reversed(range(h)):
-        if j>i:
-            print(' ',end='')
-        elif j==i:
-            print('*',end='')
-        elif j<i:
-            print('*',end='')
-    print()
+for i in range(1,101):
+    if i%3==0 and i%5==0:
+        print('FizzBuzz')
+    elif i%3==0:
+        print('Fizz')
+    elif i%5==0:
+        print('Buzz')
+    else:
+        print(i)
+
+for i in range(1,101):
+    if i%15==0:
+        print('FizzBuzz')
+    elif i%3==0:
+        print('Fizz')
+    elif i%5==0:
+        print('Buzz')
+    else:
+        print(i)
+
+for i in range(1,101):
+    print('Fizz'*(i%3==0)+'Buzz'*(i%5==0) or i)
+        # 뭘 출력*언제 출력 + 뭘 출력*언제 출력
+          # 둘 다 True면 앞 합쳐서 출력 둘 다 false면 뒤의 i
