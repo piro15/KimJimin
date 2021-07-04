e0abbdded710f9de730c6e7554a74de57a7d8721
Jimin Kim <0305jimin@gmail.com>
2021-07-04 12:40:54 +0900

    16.2

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,8 +1,4 @@
-x=int(input())
-
-if 11<=x<=20:
-    print('11~20')
-elif 21<=x<=30:
-    print('21~30')
-else:
-    print('아무것도 해당하지 않음')
+count = int(input('반복할 횟수를 입력하세요: '))
+ 
+for i in range(count):
+    print('Hello, world!', i)
