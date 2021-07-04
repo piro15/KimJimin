146dbb6b221b49c498cfe6ab0ce3cada74c0769d
Jimin Kim <0305jimin@gmail.com>
2021-07-04 16:28:08 +0900

    Unit 20

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,24 +1,9 @@
 for i in range(1,101):
-    if i%3==0 and i%5==0:
+    if i%22==0:
         print('FizzBuzz')
-    elif i%3==0:
+    elif i%2==0:
         print('Fizz')
-    elif i%5==0:
+    elif i%11==0:
         print('Buzz')
     else:
         print(i)
-
-for i in range(1,101):
-    if i%15==0:
-        print('FizzBuzz')
-    elif i%3==0:
-        print('Fizz')
-    elif i%5==0:
-        print('Buzz')
-    else:
-        print(i)
-
-for i in range(1,101):
-    print('Fizz'*(i%3==0)+'Buzz'*(i%5==0) or i)
-        # 뭘 출력*언제 출력 + 뭘 출력*언제 출력
-          # 둘 다 True면 앞 합쳐서 출력 둘 다 false면 뒤의 i
