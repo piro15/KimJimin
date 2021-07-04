7b86de8b93f25992f00ac52b0c28a8f1b871b742
Jimin Kim <0305jimin@gmail.com>
2021-07-04 15:24:05 +0900

    Unit 19

--- 파이썬 과제/hello.py
+++ 파이썬 과제/hello.py
@@ -1,9 +1,7 @@
-i=0
-while True:
-    if i%10 !=3 :
-        i+=1
-        continue
-    if i>73:
-        break
-    print(i, end=' ')
-    i+=1
+for i in range(5):
+    for j in range(5):
+        if j<i:
+            print(' ',end='')
+        else:
+            print('*',end='')
+    print()
