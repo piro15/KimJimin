628ce30e984e88d55aa7c38e314dbb29ce64a759
Jimin Kim <0305jimin@gmail.com>
2021-07-07 15:28:23 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,42 @@
 num=0
 a=0
-while a==0:
-        try:
-            n=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):"))
+go_on=1
+try:
+    while 1:
+        while a==0:
+                try:
+                    n=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):"))
 
-            if n!=1 and n!=2 and n!=3 :
-                print('1,2,3 중 하나를 입력하세요')
-            else:
-                for i in range(n) :
-                    num+=1
-                    print('playerA:',num)
-                n=0
-                a=1
-        except ValueError:
-            print('정수를 입력하세요.')
-a=0
+                    if n!=1 and n!=2 and n!=3 :
+                        print('1,2,3 중 하나를 입력하세요')
+                    else:
+                        for i in range(n) :
+                            num+=1
+                            print('playerA:',num)
+                            if num==31:
+                                raise Exception
+                        n=0
+                        a=1
+                except ValueError:
+                    print('정수를 입력하세요.')
+        a=0
 
-while a==0:
-        try:
-            n=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):"))
+        while a==0:
+                try:
+                    n=int(input("부를 숫자의 개수를 입력하세요(1,2,3만 입력 가능):"))
 
-            if n!=1 and n!=2 and n!=3 :
-                print('1,2,3 중 하나를 입력하세요')
-            else:
-                for i in range(n) :
-                    num+=1
-                    print('playerB:',num)
-                n=0
-                a=1
-        except ValueError:
-            print('정수를 입력하세요.')
-
+                    if n!=1 and n!=2 and n!=3 :
+                        print('1,2,3 중 하나를 입력하세요')
+                    else:
+                        for i in range(n) :
+                            num+=1
+                            print('playerB:',num)
+                            if num==31:
+                                raise GameOver
+                        n=0
+                        a=1
+                except ValueError:
+                    print('정수를 입력하세요.')
+        a=0
+except Exception:
+    print()
