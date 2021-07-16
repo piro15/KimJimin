88aa4d52d273e7cf275d069eb21fa7278eeab856
Jimin Kim <0305jimin@gmail.com>
2021-07-16 18:15:33 +0900

    07 적절한 HTTP 상태코드로 응답하기

--- AskdjangoBasic/askcompany/shop/urls.py
+++ AskdjangoBasic/askcompany/shop/urls.py
@@ -7,7 +7,17 @@ app_name = 'shop'
 
 urlpatterns = [
     path('archives/<yyyy:year>/', views.archives_year),
-    path('items/', item_list, name='item_list'),
-    # ~~items/라는 주소에 매칭이되면 item_list라는 이름의 함수를 호춣을 하겠다. 이때 이런 패턴의 이름을 item_list라고 하겠다.
+    # path('items/', item_list, name='item_list'),
+    # # ~~items/라는 주소에 매칭이되면 item_list라는 이름의 함수를 호춣을 하겠다. 이때 이런 패턴의 이름을 item_list라고 하겠다.
 
 ]
+
+
+# HTTP 상태코드:
+# 모두 다 200으로 통일시키는 곳도 있지만 경우에 맞게 각각 다른 응답을 보내는 것이 좋다. REST API에 특히 좋다는데 과제 다 하고 REST API가 뭔지 찾아보기
+# HttpResopnse 쓸 때 status 변경하고 싶으면 콤마 쓰고 스테이터스 =~~.
+
+# resolve_url('shop:item_list') 패턴네임이 item_list인 애의 url을 찾아준다
+# resolve_url 쓰는 두 줄이 redirect쓰는 한 줄과 같다. 그러니 후자 쓰는 게 더 좋다.
+
+# Http404쓸 거면 raise. HttpResponseNotFound 쓸 거면 return. 하지만 후자는 잘 쓰지X
--- AskdjangoBasic/askcompany/shop/views.py
+++ AskdjangoBasic/askcompany/shop/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from shop.models import Item
+# from shop.models import Item
 
 # Create your views here.
 
 
-def item_list(request):  # 함수를 만들어서
-    # 아이템이라는 클래스를 통해 데이터베이스 접근 가능. 모든 아이템 목록을 가져와라X 가져올 준비 해라. 실제 가져오는 건 나중에 쿼리셋으로 레코드에 접근할 때.
-    qs = Item.objects.all()
-    return render(request, 'shop/item_list.html', {  # httpresponse 타입의 객체 생성. 함수를 통해.
-        'item_list': qs
-    })
+# def item_list(request):  # 함수를 만들어서
+#     # 아이템이라는 클래스를 통해 데이터베이스 접근 가능. 모든 아이템 목록을 가져와라X 가져올 준비 해라. 실제 가져오는 건 나중에 쿼리셋으로 레코드에 접근할 때.
+#     qs = Item.objects.all()
+#     return render(request, 'shop/item_list.html', {  # httpresponse 타입의 객체 생성. 함수를 통해.
+#         'item_list': qs
+#     })
 
 
 def archives_year(request, year):
