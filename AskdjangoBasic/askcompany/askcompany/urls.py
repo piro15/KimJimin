3c14dcc82460900588001b2a56081774b47672d3
Jimin Kim <0305jimin@gmail.com>
2021-07-16 14:11:20 +0900

    04 VSCode 장고 디버깅 세팅하기

--- AskdjangoBasic/askcompany/askcompany/urls.py
+++ AskdjangoBasic/askcompany/askcompany/urls.py
@@ -1,21 +1,17 @@
-"""askcompany URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+
+def mysum(request, x, y):
+    result = x+y+100
+    return HttpResponse('result = {}'.format(result))
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('mysum/<int:x>/<int:y>/', mysum),
 ]
+# 중단점 만들고 페이지 새로고침하면 응답X 실행 중인 부분 노란색으로 표시 . 디버깅에서 request, x,y값 보기 가능.
+# 뭔가를 수정했으면 초록색 동그란 화살표 다시 누르기
