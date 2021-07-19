4262dd470eff44e90ab2a165513950323d8a1cc5
Jimin Kim <0305jimin@gmail.com>
2021-07-19 22:55:10 +0900

    (중급편) 3강

--- AskdjangoBasic/askdjango/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/askdjango/accounts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required  # 뷰가 호출돼서 프로필 템플릿 보여주는 건 로그인 된 상황에 국한되게.
+# 로그인 안된 상태면 로그인 페이지로.
+def profile(request):
+    return render(request, 'accounts/profile.html')
