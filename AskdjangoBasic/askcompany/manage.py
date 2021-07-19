ebc7aea4154c8275425ee120e4774a7659ab00a4
Jimin Kim <0305jimin@gmail.com>
2021-07-19 18:11:42 +0900

    31 다양한 구동환경을 위한 settings / requirements.txt 분기

--- AskdjangoBasic/askcompany/manage.py
+++ AskdjangoBasic/askcompany/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 # 장고 관리의 진입점
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings')
+    # 꼭 써줘야 한다 DJANGO SETTINGS MODULE~오류 뜨면 이거 확인.
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'askcompany.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
