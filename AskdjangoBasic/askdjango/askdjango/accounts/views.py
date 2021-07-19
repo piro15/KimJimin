2fcc7c8c59a7d3e1241e2925ce138463de435176
Jimin Kim <0305jimin@gmail.com>
2021-07-20 02:04:54 +0900

    (중급편) 8강

--- AskdjangoBasic/askdjango/askdjango/accounts/views.py
+++ AskdjangoBasic/askdjango/askdjango/accounts/views.py
@@ -15,12 +15,14 @@ from .forms import SignupForm
 #         if form.is_valid():
 #             user = form.save()
 #             auth_login(request, user)
-#             return redirect('profile')  # 회원가입 제대로 했으면 로그인 페이지
+#             next_url = request.GET.get('next') or 'profile'
+#             return redirect(next_url)  # 회원가입 제대로 했으면 로그인 페이지
 #     else:
 #         form = SignupForm()
 #     return render(request, 'accounts/signup.html', {
 #         'form': form,
 #     })
+
 
 class SignupView(CreateView):
     model = User
@@ -28,7 +30,8 @@ class SignupView(CreateView):
     template_name = 'accounts/signup.html'
 
     def get_success_url(self):
-        return resolve_url('profile')
+        next_url = self.request.GET.get('next') or 'profile'
+        return resolve_url(next_url)
 
     def form_valid(self, form):
         user = form.save()
