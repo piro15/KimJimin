7646c04cf7c771ce41260e34ee587fb5eba10837
Jimin Kim <0305jimin@gmail.com>
2021-07-19 20:35:57 +0900

    (중급편) 1강

---
+++ AskdjangoBasic/askdjango/askdjango/askdjango/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.shortcuts import redirect
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
+    path('', lambda req: redirect('/blog/')),
+]
