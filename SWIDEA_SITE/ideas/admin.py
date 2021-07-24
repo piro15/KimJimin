212063f9f47f756a62b9824ad2b1b792397347d9
Jimin Kim <0305jimin@gmail.com>
2021-07-24 15:31:24 +0900

    ~startapp, settings.py 추가

--- SWIDEA_SITE/ideas/admin.py
+++ SWIDEA_SITE/ideas/admin.py
@@ -3,6 +3,16 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.Idea)
-class CustomIdeaAdmin(admin.ModelAdmin):
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Game)
+class CustomUserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Card)
+class CustomUserAdmin(admin.ModelAdmin):
     pass
