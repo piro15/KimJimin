f91b8fc23b29d68b6b600cf0c150b7f7750ddde0
Jimin Kim <0305jimin@gmail.com>
2021-07-24 15:35:21 +0900

    !
    잘못 커밋

--- SWIDEA_SITE/ideas/admin.py
+++ SWIDEA_SITE/ideas/admin.py
@@ -3,16 +3,6 @@ from . import models
 # Register your models here.
 
 
-@admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Game)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(models.Card)
-class CustomUserAdmin(admin.ModelAdmin):
+@admin.register(models.Idea)
+class CustomIdeaAdmin(admin.ModelAdmin):
     pass
