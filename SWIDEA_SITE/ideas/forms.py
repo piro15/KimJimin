d7824b9badcb4bdd9ebf6bebd2185a3981336c2a
Jimin Kim <0305jimin@gmail.com>
2021-08-02 22:59:47 +0900

    [Design] idea_form

--- SWIDEA_SITE/ideas/forms.py
+++ SWIDEA_SITE/ideas/forms.py
@@ -6,3 +6,21 @@ class IdeaForm(forms.ModelForm):
     class Meta:
         model = Idea
         fields = '__all__'
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control',
+                'style': 'border-radius: 10px;'
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': 'form-control',
+                'style': 'border-radius: 10px;',
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'form-control',
+                'style': 'border-radius: 10px;'
+            }),
+            # 'interest': forms.NumberInput(attrs={
+            #     'class': 'form-control',
+            #     'style': 'border-radius: 10px; '
+            #     'width: 65px;'})
+        }
